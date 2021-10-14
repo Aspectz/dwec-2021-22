8feddd136b527d4d496f746a728a87113dc10e71
Josep Garzó Sánchez <josepgarzo7@gmail.com>
2021-10-14 18:42:01 +0200

    Prueba api pokimon

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 import {Login} from './login.js';
+import {Main} from './main.js';
 
 (()=>{
     function login(container){
         let login=new Login();
         login.renderLogin(container);
     }
-
+    function main(container){
+        let main=new Main();
+        main.renderMain(container);
+    }
 
 
     document.addEventListener("DOMContentLoaded",function(){
         let container=document.querySelector("#container");
-        login(container);
+        main(container);
         console.log("a");
     });
 })();
