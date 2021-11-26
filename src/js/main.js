a38077d8fa337ed863ce31b3666b0d5407416151
Josep Garzó Sánchez <josepgarzo7@gmail.com>
2021-11-26 08:03:21 +0100

    a

--- src/js/main.js
+++ src/js/main.js
@@ -9,39 +9,44 @@ class Main {
     this.container=cont;
   }
   renderMain() {
-    getPosts(container);
+    if(localStorage.getItem("localId")!=null)
+      this.getPosts();
+    else{
+      this.getAllPosts();
+    }
+  }
+  async getAllPosts(container){
+    let resp=await fetch("https://projectjs-b6bfe-default-rtdb.europe-west1.firebasedatabase.app/posts.json");
+    let data=await resp.json();
+    let posts=data;
+    this.container.innerHTML="";
+    this.container.classList.add("mainContainer");
+  
+    //Container where all posts are showed to user
+    let divPosts=document.createElement("div");
+    divPosts.classList.add("containerPosts");
+  
+    
+    //Right Aside bar
+    let divAsideRight=document.createElement("div");
+    let h1Aside=document.createElement("h1");
+    divAsideRight.classList.add("divRightAside");
+    h1Aside.innerHTML="Aside here";
+    divAsideRight.append(h1Aside);
+    
+  
+  
+    for(let post in posts){
+      posts[post].id=post;
+      let newPost=new Post(posts[post]);
+      await newPost.renderPost(divPosts);
+    }
+  
+    this.container.append(divPosts);
+    this.container.append(divAsideRight);
   }
 }
 
-async function getPosts(container){
-  let resp=await fetch("https://projectjs-b6bfe-default-rtdb.europe-west1.firebasedatabase.app/posts.json");
-  let data=await resp.json();
-  let posts=data;
-  container.innerHTML="";
-  container.classList.add("mainContainer");
 
-  //Container where all posts are showed to user
-  let divPosts=document.createElement("div");
-  divPosts.classList.add("containerPosts");
-
-  
-  //Right Aside bar
-  let divAsideRight=document.createElement("div");
-  let h1Aside=document.createElement("h1");
-  divAsideRight.classList.add("divRightAside");
-  h1Aside.innerHTML="Aside here";
-  divAsideRight.append(h1Aside);
-  
-
-
-  for(let post in posts){
-    posts[post].id=post;
-    let newPost=new Post(posts[post]);
-    await newPost.renderPost(divPosts);
-  }
-
-  container.append(divPosts);
-  container.append(divAsideRight);
-}
 
   
